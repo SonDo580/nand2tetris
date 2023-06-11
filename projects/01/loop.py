9ceed8ef9868dd996d3a5cac77ea3cd1cf109d93
Son Do <dominhson580@gmail.com>
2023-06-11 15:39:47 +0700

    Finish project 1 - build logic gates

---
+++ projects/01/loop.py
@@ -0,0 +1,30 @@
+# Not16
+# for i in range(16):
+#     print(f"Not(in=in[{i}], out=out[{i}]);")
+
+# And16
+# for i in range(16):
+# print(f"And(a=a[{i}], b=b[{i}],out=out[{i}]);")
+
+# Or16
+# for i in range(16):
+#     print(f"Or(a=a[{i}], b=b[{i}],out=out[{i}]);")
+
+# Mux16
+# for i in range(16):
+#     print(f"Mux(a=a[{i}], b=b[{i}], sel=sel, out=out[{i}]);")
+
+
+# Or8Way
+# def getA(i):
+#     if i == 0:
+#         return "in[0]"
+#     else:
+#         return f"res{i - 1}"
+# def getOut(i):
+#     if i == 6:
+#         return "out"
+#     else:
+#         return f"res{i}"
+# for i in range(7):
+#     print(f"Or(a={getA(i)}, b=in[{i + 1}], out={getOut(i)});")
