4739e0d8afc8555471184892e38625b637cc4f0b
Son Do <dominhson580@gmail.com>
2023-06-13 21:49:35 +0700

    Build arithmetic chips

---
+++ projects/02/loop.py
@@ -0,0 +1,9 @@
+# Add16
+# print(f"HalfAdder(a=a[0], b=b[0], sum=out[0], carry=carry0);")
+# for i in range(1, 16):
+#     print(f"FullAdder(a=a[{i}], b=b[{i}], c=carry{i-1}, sum=out[{i}], carry=carry{i});")
+
+# Inc16
+# print(f"HalfAdder(a=in[0], b=true, sum=out[0], carry=carry0);")
+# for i in range(1, 16):
+#     print(f"HalfAdder(a=in[{i}], b=carry{i-1}, sum=out[{i}], carry=carry{i});")
