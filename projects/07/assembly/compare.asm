b47c59d4a83a325b05b5230f31cbf3c1e2db3cf8
Son Do <dominhson580@gmail.com>
2023-09-28 21:55:17 +0700

    Change comparison command implementation (labels)

--- projects/07/assembly/compare.asm
+++ projects/07/assembly/compare.asm
@@ -11,34 +11,34 @@ M=M-1   // SP--
 A=M     // go to x
 D=M-D   // D = x - y
 
-@TRUE-label
+@TRUE
 D;JEQ   // D == 0 => x == y => jump to TRUE
 
 @SP
 A=M     // go to x
 M=0     // set x = false
 
-@CONTINUE-label
+@CONTINUE
 0;JMP   // jump to CONTINUE
 
-(TRUE-label)
+(TRUE)
 @SP
 A=M     // go to x
 M=-1    // set x = true
 
-(CONTINUE-label)
+(CONTINUE)
 @SP
 M=M+1   // SP++
 
 // Implement: gt
 // ...similar
-@TRUE-label
+@TRUE
 D;JGT   // D > 0 => x > y => jump to TRUE
 // ...similar
 
 
 // Implement: lt
 // ...similar
-@TRUE-label
+@TRUE
 D;JLT   // D < 0 => x < y => jump to TRUE
 // ...similar
