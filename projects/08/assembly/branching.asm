2468818ad6cae5fe3727ba7bebcb3a5478d72596
Son Do <dominhson580@gmail.com>
2023-09-29 21:31:54 +0700

    Fix if-goto implementation

--- projects/08/assembly/branching.asm
+++ projects/08/assembly/branching.asm
@@ -11,8 +11,5 @@ M=M-1   // SP--
 A=M     // go to address stored in SP
 D=M     // D = RAM[SP]
 
-@SP
-M=M-1   // SP-- (remove 'condition' from stack)
-
 @LABEL
 D;JNE   // jump to LABEL if D != false
