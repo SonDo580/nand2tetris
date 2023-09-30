53650d8cc8a9dc8138816cab8e1b016c2b6f3744
Son Do <dominhson580@gmail.com>
2023-09-30 17:00:19 +0700

    Handle branching and function commands for 1 file

--- projects/08/utils.py
+++ projects/08/utils.py
@@ -2,14 +2,18 @@
 SEGMENT_POINTERS = {"local": "LCL", "argument": "ARG", "this": "THIS", "that": "THAT"}
 ARITHMETIC_LOGIC_COMMANDS = ["add", "sub", "neg", "and", "or", "not"]
 COMPARISON_COMMANDS = ["eq", "gt", "lt"]
+BRANCHING_COMMANDS = ["label", "goto", "if-goto"]
+FUNCTION_COMMANDS = ["function", "call", "return"]
 
 # Common ASM blocks
 decrement_SP = "@SP\nM=M-1\n"
 increment_SP = "@SP\nM=M+1\n"
 load_A_with_M = "A=M\n"
-load_D_with_M = "D=M\n"
 load_M_with_D = "M=D\n"
+load_D_with_M = "D=M\n"
+load_D_with_A = "D=A\n"
 load_SP_to_A = "@SP\nA=M\n"
+unconditional_jump = "0;JMP\n"
 
 # Produce Hack assembly from Arithmetic/Logic command
 ARITHMETIC_LOGIC_OPERATIONS = {
@@ -166,20 +170,131 @@ def push_pop_to_asm(command, name_space):
         )
 
 
-# This function produce Hack assembly from a VM command
+# Produce Hack assembly from branching command
+def branching_to_asm(command, label):
+    if command == "label":
+        return f"({label})\n"
+
+    if command == "goto":
+        return f"@{label}\n" + unconditional_jump
+
+    if command == "if-goto":
+        return decrement_SP + load_A_with_M + load_D_with_M + f"@{label}\n" + "D;JNE\n"
+
+
+# Produce Hack assembly from return command
+def restore_value(label, offset):
+    return f"""@{offset}
+D=A
+@endFrame
+D=M-D
+A=D
+D=M
+@{label}
+M=D    
+"""
+
+
+def produce_return_asm():
+    get_end_frame = "@LCL\n" + load_D_with_M + "@endFrame\n" + load_M_with_D
+    get_return_address = restore_value("returnAddress", 5)
+    save_return_value = (
+        decrement_SP
+        + load_A_with_M
+        + load_D_with_M
+        + "@ARG\n"
+        + load_A_with_M
+        + load_M_with_D
+    )
+    reposition_SP = "@ARG\n" + load_D_with_M + "@SP\n" + "M=D+1\n"
+    restore_that = restore_value("THAT", 1)
+    restore_this = restore_value("THIS", 2)
+    restore_arg = restore_value("ARG", 3)
+    restore_lcl = restore_value("LCL", 4)
+    goto_returnAddress = "@returnAddress\n" + load_A_with_M + unconditional_jump
+
+    return (
+        get_end_frame
+        + get_return_address
+        + save_return_value
+        + reposition_SP
+        + restore_that
+        + restore_this
+        + restore_arg
+        + restore_lcl
+        + goto_returnAddress
+    )
+
+
+# Produce Hack assembly from function command
+def set_iteration(count):
+    return f"""@{count}
+D=A
+@i
+M=D
+"""
+
+
+def init_local_variables(function_name):
+    return f"""({function_name}$init)
+@SP
+A=M
+M=0    
+@SP
+M=M+1
+@i
+M=M-1
+D=M
+@{function_name}$init
+D;JNE     
+"""
+
+
+def produce_function_asm(line):
+    words = line.split(" ")
+    function_name = words[1]
+    num_args = words[2]
+
+    return (
+        f"({function_name})\n"
+        + set_iteration(num_args)
+        + init_local_variables(function_name)
+    )
+
+
+# Produce Hack assembly from function-related command
+def function_commands_to_asm(line):
+    words = line.split(" ")
+    command = words[0]
+
+    if command == "return":
+        return produce_return_asm()
+
+    if command == "function":
+        return produce_function_asm(line)
+
+
+# Produce Hack assembly from a VM command
 comparison_label_count = 0
 
 
 def to_asm(line, name_space):
     words = line.split(" ")
+    command = words[0]
 
-    if words[0] in ["push", "pop"]:
+    if command in ["push", "pop"]:
         return push_pop_to_asm(line, name_space)
 
-    elif words[0] in ARITHMETIC_LOGIC_COMMANDS:
-        return arithmetic_logic_to_asm(words[0])
+    if command in ARITHMETIC_LOGIC_COMMANDS:
+        return arithmetic_logic_to_asm(command)
 
-    elif words[0] in COMPARISON_COMMANDS:
+    if command in COMPARISON_COMMANDS:
         global comparison_label_count
         comparison_label_count += 1
-        return comparison_to_asm(words[0], comparison_label_count)
+        return comparison_to_asm(command, comparison_label_count)
+
+    if command in BRANCHING_COMMANDS:
+        return branching_to_asm(command, words[1])
+
+    if command in FUNCTION_COMMANDS:
+        return function_commands_to_asm(line)
