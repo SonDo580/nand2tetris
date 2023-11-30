d20b9ed10d0a441fc5d1d883b7b2d709b1ecd03d
Son Do <dominhson580@gmail.com>
2023-11-30 14:54:54 +0700

    Create shell script to compare .xml files

---
+++ projects/10/compare.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Specify the source and output directories
+source_dir="."
+output_dir="output"
+exclude_folders="output __pycache__"
+text_comparer='../../tools/TextComparer.sh'
+
+# Variable to track whether a failure has occurred
+failure_found=false
+
+# Iterate over folders in the source directory
+for source_folder in "$source_dir"/*; do
+    # Extract the folder name
+    folder_name=$(basename "$source_folder")
+
+    # Check if the item is a directory and not in the exclusion list
+    if [ -d "$source_folder" ]; then
+        should_exclude=false
+        for excluded_folder in $exclude_folders; do
+            if [ "$folder_name" = "$excluded_folder" ]; then
+                should_exclude=true
+                break
+            fi
+        done
+
+        if [ "$should_exclude" = false ]; then
+            # Construct the corresponding output folder path
+            output_folder="$output_dir/$folder_name"
+
+            # Check if the output folder exists
+            if [ -d "$output_folder" ]; then
+                # Iterate over XML files in the source folder
+                for source_file in "$source_folder"/*.xml; do
+                    # Extract the file name (without extension)
+                    filename=$(basename "$source_file" .xml)
+
+                    # Construct the corresponding output file path
+                    output_file="$output_folder/$filename.xml"
+
+                    # Check if the output file exists
+                    if [ -e "$output_file" ]; then
+                        # Print the program name and file being compared
+                        echo "--Checking: $folder_name/$filename"
+
+                        # Compare the XML files
+                        sh "$text_comparer" "$source_file" "$output_file"
+
+                        # Check the exit status of the TextComparer
+                        if [ $? -ne 0 ]; then
+                            echo "Comparison failure in $output_file"
+                            failure_found=true
+                            break
+                        fi
+                    else
+                        echo "--Output file $output_file not found."
+                    fi
+                done
+            else
+                echo "--Output folder $output_folder not found."
+            fi
+        fi
+    fi
+
+    # Exit the loop if a failure is found
+    if [ "$failure_found" = true ]; then
+        break
+    fi
+done
+
+# Exit the script with a failure code if a failure is found
+if [ "$failure_found" = true ]; then
+    exit 1
+fi
