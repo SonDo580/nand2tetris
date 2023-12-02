18af5f36ef6b3db748c0f47c3b55bba9e104a94e
Son Do <dominhson580@gmail.com>
2023-12-02 16:52:58 +0700

    Update constants

--- projects/11/constants.py
+++ projects/11/constants.py
@@ -56,6 +56,8 @@ SYMBOLS = {
     "~",
 }
 
+KEYWORD_CONSTANTS = {"true", "false", "null", "this"}
+
 # Unary operators
 UNARY_OPS = {"-", "~"}
 
@@ -70,4 +72,57 @@ BINARY_OPS = {
     ESCAPED["<"],
     ESCAPED[">"],
     "=",
+}
+
+# OS Classes
+OS_CLASSES = {
+    "Math",
+    "Memory",
+    "Screen",
+    "Output",
+    "Keyboard",
+    "String",
+    "Array",
+    "Sys",
+}
+
+# Variable kinds
+FIELD = "field"
+STATIC = "static"
+ARG = "argument"
+VAR = "local"
+
+# VM virtual segments
+CONSTANT = "constant"
+ARGUMENT = "argument"
+LOCAL = "local"
+STATIC = "static"
+THIS = "this"
+THAT = "that"
+POINTER = "pointer"
+TEMP = "temp"
+
+# VM commands
+ADD = "add"
+SUB = "sub"
+NEG = "neg"
+EQ = "eq"
+GT = "gt"
+LT = "lt"
+AND = "and"
+OR = "or"
+NOT = "not"
+
+# Unary operator to VM command
+UNARY_COMMAND = {"-": NEG, "~": NOT}
+
+# Binary operator to VM command
+BINARY_COMMAND = {
+    "+": ADD,
+    "-": SUB,
+    "=": EQ,
+    ESCAPED[">"]: GT,
+    ESCAPED["<"]: LT,
+    ESCAPED["&"]: AND,
+    "|": OR,
 }
