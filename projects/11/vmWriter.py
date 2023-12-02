6e559bf01f0886ccbb9c274be309426578c0a31f
Son Do <dominhson580@gmail.com>
2023-12-02 16:46:48 +0700

    Fix the VM Writer

--- projects/11/vmWriter.py
+++ projects/11/vmWriter.py
@@ -3,28 +3,28 @@ class VMWriter:
         self._output_file = output_file
 
     def writePush(self, segment, index):
-        self._output_file(f"push {segment} {index}\n")
+        self._output_file.write(f"push {segment} {index}\n")
 
     def writePop(self, segment, index):
-        self._output_file(f"pop {segment} {index}\n")
+        self._output_file.write(f"pop {segment} {index}\n")
 
     def writeArithmetic(self, command):
-        self._output_file(f"{command}\n")
+        self._output_file.write(f"{command}\n")
 
     def writeLabel(self, label):
-        self._output_file(f"label {label}\n")
+        self._output_file.write(f"label {label}\n")
 
     def writeGoto(self, label):
-        self._output_file(f"goto {label}\n")
+        self._output_file.write(f"goto {label}\n")
 
     def writeIf(self, label):
-        self._output_file(f"if-goto {label}\n")
+        self._output_file.write(f"if-goto {label}\n")
 
     def writeCall(self, func, num_vars):
-        self._output_file(f"call {func} {num_vars}\n")
+        self._output_file.write(f"call {func} {num_vars}\n")
 
     def writeFunction(self, func, num_vars):
-        self._output_file(f"function {func} {num_vars}\n")
+        self._output_file.write(f"function {func} {num_vars}\n")
 
     def writeReturn(self):
-        self._output_file("return\n")
+        self._output_file.write("return\n")
